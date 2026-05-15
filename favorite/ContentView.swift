b9fc9b670a5a9d89d7f81bf7befdc7f2a18d4717
sin-ami <amikojima12@gmail.com>
2026-05-16 08:56:17 +0900

    Initial Commit

---
+++ favorite/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  favorite
+//
+//  Created by 小島愛実 on 2026/05/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ favorite/favoriteApp.swift
@@ -0,0 +1,17 @@
+//
+//  favoriteApp.swift
+//  favorite
+//
+//  Created by 小島愛実 on 2026/05/16.
+//
+
+import SwiftUI
+
+@main
+struct favoriteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
